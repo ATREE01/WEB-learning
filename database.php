bf81d7d769c71fbbbc4c3e6a54d79896e7cca078
ATREE01 <anyu9898@yahoo.com>
2022-11-19 09:23:08 +0800

    database.php FIX

--- database.php
+++ database.php
@@ -3,8 +3,10 @@
     define('DB_USER','root');
     define('DB_PASS',"");
     define("DB_NAME",'anime_web');
-    $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
     if($mysqli-> connect_errno){
         die("connect error:".$mysqli->connect_errno);
     }
